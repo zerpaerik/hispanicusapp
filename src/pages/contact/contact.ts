b76833a89a8f49d4feab5ec8de96285d5cb04916
unknown <erik>
2018-05-06 21:09:38 -0400

    emails

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { ToastController } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+import { LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -11,19 +14,62 @@ export class ContactPage {
 
 	msg : string;
 
-  constructor(public authProvider : AuthProvider, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public loadCtrl : LoadingController, public translate : TranslateService, public toastCtrl : ToastController, public authProvider : AuthProvider, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   sendMessage(){
   	if (this.msg.length > 10) {
+  		let load = this.presentLoading();
+  		load.present();
   		this.authProvider.sendMessage(this.msg)
   			.subscribe(res => {
-  				console.log("Mensaje Enviado");
+  				
   				this.msg = "";
+  				this.presentToast();  				
+					this.navCtrl.pop();
+					load.dismiss();
+
   			}, error => {
+  				load.dismiss();
   				console.log(error);
   			});
   	}
+  }
+
+  presentLoading() {
+
+  	let msg = "";
+
+  	this.translate.get('HOME.CONTACT').subscribe(res => {
+  		msg = res.SENDING;
+  	}, error => {
+  		msg = "Enviando";
+  	});
+
+    let loader = this.loadCtrl.create({
+      spinner : 'crescent',
+      content : msg, 
+      showBackdrop : false
+    });
+    return loader;
+  }
+
+  presentToast(){
+  	
+  	let msg = "";
+
+  	this.translate.get('HOME.CONTACT').subscribe(res => {
+  		msg = res.SENT;
+  	}, error => {
+  		msg = "Su mensaje ha sido enviado";
+  	});
+
+  	let toast = this.toastCtrl.create({
+ 	    message: msg,
+      duration: 3000,
+      position: 'middle'
+		});
+  	toast.present();
   }
 
 }
